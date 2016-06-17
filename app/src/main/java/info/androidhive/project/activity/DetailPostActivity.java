19854406c3478beb81788ceadefc0a8162eded56
AnhTVc <tranvietanh.hust@gmail.com>
2016-06-18 00:30:27 +0700

    AnhTVc commit date 16/6

--- app/src/main/java/info/androidhive/project/activity/DetailPostActivity.java
+++ app/src/main/java/info/androidhive/project/activity/DetailPostActivity.java
@@ -16,6 +16,7 @@ import info.androidhive.project.model.ThreeImage;
 public class DetailPostActivity extends AppCompatActivity {
     ImageAdapter adapter = null;
     ArrayList<ThreeImage> arrayOfElement = new ArrayList<ThreeImage>();
+    ArrayList<ThreeImage> tempAdapter = new ArrayList<ThreeImage>();
     Element element = null;
 
     @Override
@@ -23,25 +24,51 @@ public class DetailPostActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_post);
 
+        adapter = new ImageAdapter(this, arrayOfElement);
         ThreeImage threeImage;
         ArrayList<Image> images;
+        int postion = 0;
         //TODO
-        for (int i = 0; i < (element.getPost().getImages().size() % 3); i++) {
-            images = new ArrayList<>();
-            images.add(element.getPost().getImages().get(i));
+        for (int i = 1; i < (element.getPost().getImages().size() % 3) + 1; i++) {
             threeImage = new ThreeImage();
+            images = new ArrayList<>();
+            for (int j = 0; j <= 3; j++) {
+                postion = j * i - 1;
+                images.add(element.getPost().getImages().get(i * j - 1));
+                if (j == 3) {
+                    threeImage.setThreeImage(images);
+                    tempAdapter.add(threeImage);
+                    images.clear();
+                }
+            }
         }
-        adapter = new ImageAdapter(this, arrayOfElement);
 
+        if (postion + 1 < element.getPost().getImages().size()) {
+            threeImage = new ThreeImage();
+            for (int i = postion + 1; i < element.getPost().getImages().size(); i++) {
+                images = new ArrayList<>();
+                images.add(element.getPost().getImages().get(i));
+                threeImage.setThreeImage(images);
+                tempAdapter.add(threeImage);
+                images.clear();
+            }
+        }
+
+        process();
 
     }
 
     private void process() {
         ListView listImageInDetail = (ListView) this.findViewById(R.id.listImageInDetail);
         if (listImageInDetail.getAdapter() == null) {
-
+            for (int i = 0; i < tempAdapter.size(); i++) {
+                adapter.add(tempAdapter.get(i));
+            }
         } else {
-
+            for (int i = 0; i < tempAdapter.size(); i++) {
+                adapter.add(tempAdapter.get(i));
+                adapter.notifyDataSetChanged();
+            }
         }
     }
 }
